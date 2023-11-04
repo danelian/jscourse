fda37ed3c147e0034fc582a61663fe1e6e4abbc1
danelian <141317269+danelian@users.noreply.github.com>
2023-11-04 04:41:26 +0300

    13. arrays

---
+++ js/13-arrays.js
@@ -0,0 +1,9 @@
+// ============ 7. Массивы =============
+
+const autoBrands = ['Audi', 'BMW', 'Mazda', 'Toyota'];
+
+console.log(autoBrands);
+console.log(autoBrands[0]);
+console.log(autoBrands[2]);
+
+console.log(autoBrands.length);
