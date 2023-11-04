1d6876089dc53951111c46f9d6887a66e7d2a415
danelian <141317269+danelian@users.noreply.github.com>
2023-11-04 03:12:10 +0300

    07. functions

---
+++ js/07-functions.js
@@ -0,0 +1,16 @@
+// ===================== 5. Функции =======================
+
+function sayHi() {
+  console.log('Barev, hargayin mard!');
+}
+sayHi();
+
+// function declaration
+function sayHi() {
+  alert('Barev!');
+}
+
+// function expression
+const sayHi = function () {
+  alert('Barev!');
+}
