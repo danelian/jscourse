508c2c697b27486fcaa0e288155829ea1c25e6b4
danelian <141317269+danelian@users.noreply.github.com>
2023-11-04 03:21:20 +0300

    08. function params

---
+++ js/08-function-params.js
@@ -0,0 +1,9 @@
+// ========= 6.1. Параметры и аргументы для функции ===========
+
+function sayHi(name) {
+  console.log(`Barev, ${name}! Vonc es?`);
+}
+sayHi('Danil');
+
+sayHi('Armen');
+sayHi('Ashot');
